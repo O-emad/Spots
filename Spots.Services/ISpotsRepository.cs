9eaa345e9c32f6a46bdb308d48eaf7a0a02dd942
Omar Rokiba <orokiba@asu.edu>
2021-05-02 04:06:47 +0200

    added spots repository in services

--- Spots.Services/ISpotsRepository.cs
+++ Spots.Services/ISpotsRepository.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using Spots.Domain;
 
 namespace Spots.Services
 {
     public interface ISpotsRepositroy
     {
+        IEnumerable<Category> GetCategories();
+        Category GetCategoryById(Guid categoryId);
+        void AddCategory(Category category);
+        void UpdateCategory(Guid categoryId, Category category);
+        void DeleteCategory(Category category);
+        bool CategoryExists(Guid categoryId);
 
+        IEnumerable<Vendor> GetVendors();
+        Vendor GetVendorById(Guid vendorId);
+        void AddVendor(Vendor vendor);
+        void UpdateVendor(Guid vendorId, Vendor vendor);
+        void DeleteVendor(Vendor vendor);
+        bool VendorExists(Guid vendorId);
+
+        bool Save();
     }
 }
---
+++ Spots.Services/SpotsRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spots.Data;
+using Spots.Domain;
+namespace Spots.Services
+{
+    public class SpotsRepository : ISpotsRepositroy
+    {
+        private readonly SpotsContext context;
+
+        public SpotsRepository(SpotsContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void AddCategory(Category category)
+        {
+            if(category != null)
+            {
+                context.Add<Category>(category);
+            }
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            return context.Categories;
+        }
+
+        public Category GetCategoryById(Guid categoryId)
+        {
+            return context.Categories.Where(c => c.Id == categoryId).FirstOrDefault();
+        }
+
+        public void UpdateCategory(Guid categoryId, Category category)
+        {
+            //does nothing for now
+        }
+        public void DeleteCategory(Category category)
+        {
+            context.Remove(category);
+        }
+        public bool CategoryExists(Guid categoryId)
+        {
+            return context.Categories.Where(c => c.Id == categoryId).Any();
+        }
+
+
+        public void AddVendor(Vendor vendor)
+        {
+            if(vendor != null)
+            {
+                context.Add<Vendor>(vendor);
+            }
+        }
+        public Vendor GetVendorById(Guid vendorId)
+        {
+            return context.Vendors.Where(v => v.Id == vendorId).FirstOrDefault();
+        }
+
+        public IEnumerable<Vendor> GetVendors()
+        {
+            return context.Vendors.OrderBy(v => v.SortOrder);
+        }
+        public void UpdateVendor(Guid vendorId, Vendor vendor)
+        {
+            //does nothing for now
+        }
+        public void DeleteVendor(Vendor vendor)
+        {
+            context.Remove(vendor);
+        }
+
+        
+
+        public bool VendorExists(Guid vendorId)
+        {
+            return context.Vendors.Where(v => v.Id == vendorId).Any();
+        }
+
+        public bool Save()
+        {
+            return (context.SaveChanges() >= 0);
+        }
+    }
+}
