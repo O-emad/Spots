5a3c3ce707d48cf0ed09a9a48afe772c6afb71b4
Omar Rokiba <orokiba@asu.edu>
2021-06-19 04:03:18 +0200

    adding offer Qr scan

---
+++ Spots.APIs/Controllers/OfferQrScanController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Spots.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spots.APIs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OfferQrScanController : ControllerBase
+    {
+        private readonly ISpotsRepositroy repositroy;
+
+        public OfferQrScanController(ISpotsRepositroy repositroy)
+        {
+            this.repositroy = repositroy ?? throw new ArgumentNullException(nameof(repositroy));
+        }
+
+
+
+        [HttpPost("{offerId}")]
+        public IActionResult UseQrCode(Guid offerId, string vendorName)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var offer = repositroy.GetSingleOfferById(offerId);
+            if(offer == null)
+            {
+                return NotFound(new ResponseModel()
+                {
+                    StatusCode = StatusCodes.Status404NotFound,
+                    Message = "Offer not found"
+                });
+            }
+            var vendor = repositroy.GetVendorById(offer.VendorId, false);
+            if(vendor.Name != vendorName)
+            {
+                return BadRequest(new ResponseModel()
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = $"Vendor: {vendorName} doesn't have the following offer"
+                });
+            }
+            //send a push notification 
+            return NoContent();
+        }
+
+    }
+}
